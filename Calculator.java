3db5a657e1cad93a6917dbf9be54d05c15d98ec8
SOPHIA MARY R <soph20410.cs@rmkec.ac.in>
2023-10-14 12:47:28 +0530

    Implemented constructor

--- Calculator.java
+++ Calculator.java
@@ -5,15 +5,31 @@ import java.awt.event.*;
 public class Calculator implements  ActionListener{
 
     JFrame frame;
+    //JFrame => top level container that provides a window on the screen
     JTextField textField;
     JButton[] numberButtons = new JButton[10];
     JButton[] functionButtons = new JButton[8];
     JButton addButton, subButton, mulButton, divButton;
     JButton decButton, equButton, delButton, clrButton;
     JPanel panel;
+    //JPanel => container that can store a group of components
+
+    Font myFont = new Font("Ink Free", Font.BOLD, 30);
+
+    double num1=0, num2=0, result=0;
+    char operator;
 
     Calculator(){
+        frame = new JFrame("Calculator");
+        frame.setDefaultCloseOperation((JFrame.EXIT_ON_CLOSE));
+        //by default JFrame is hidden when the user closes the window
+        //so you have to define the behaviour of the application when closed
 
+        frame.setSize(420, 550);
+        frame.setLayout(null);
+        //set the layout of the container, FlowLayout, BorderLayout, GridLayout, null Layout
+
+        frame.setVisible(true);//to make the frame appear on the screen
     }
     public static void main(String[] args){
         Calculator calc = new Calculator();
